a6f0ea342ed50738431c0178a8b42ffdcc5b462e
roshan kshirsagar <roshankshirsagar1666@gmail.com>
2024-01-23 17:31:51 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Roshan Kshirsagar',
+    author_email= 'roshankshirsagar1666@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
